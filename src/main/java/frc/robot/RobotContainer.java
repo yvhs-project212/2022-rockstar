57bd24fc17f36b7b7f53917a70a277b028a1e8ce
Project212Robotics <project212robotics@gmail.com>
2022-03-16 14:21:57 -0700

    Finished with Robot Container, missing autoaim command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,18 +5,28 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.DeployIntakeCmdGroup;
 import frc.robot.commands.DriveWithJoysticksCmd;
 import frc.robot.commands.EnableFeederCmd;
 import frc.robot.commands.EnableShooterCmd;
 import frc.robot.commands.HangCmd;
+import frc.robot.commands.IntakeWithPaddlesCmd;
 import frc.robot.commands.ShooterCmd;
 import frc.robot.commands.StorageCmd;
+import frc.robot.commands.TurretCmd;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.HangSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.StorageSubsystem;
+import frc.robot.subsystems.TurretSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -45,6 +55,14 @@ public class RobotContainer {
   private final EnableFeederCmd enableFeederCmd = new EnableFeederCmd(storage);
   private final StorageCmd storageCmd = new StorageCmd(storage);
 
+  // Intake files - LL
+  public static IntakeSubsystem intake = new IntakeSubsystem();
+  private final IntakeWithPaddlesCmd intakeWithPaddlesCmd = new IntakeWithPaddlesCmd(intake);
+  private final DeployIntakeCmdGroup deployIntakeCmdGroup = new DeployIntakeCmdGroup(intake);
+  
+  // Turret files - LL
+  public static TurretSubsystem turret = new TurretSubsystem();
+  private final TurretCmd turretCmd = new TurretCmd(turret);
   
   // Controller files - LL
   public static XboxController driverJoystick = new XboxController(Constants.OI.DRIVER_NUMBER);
@@ -59,6 +77,10 @@ public class RobotContainer {
 
     driveTrain.setDefaultCommand(driveWithJoysticksCmd);   //Drive is always looking to read this command - LL
     hang.setDefaultCommand(hangCmd);
+    intake.setDefaultCommand(intakeWithPaddlesCmd);
+    shooter.setDefaultCommand(shooterCmd);
+    storage.setDefaultCommand(storageCmd);
+    turret.setDefaultCommand(turretCmd);
 
     // Configure the button bindings
     configureButtonBindings();
@@ -70,7 +92,40 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    // Driver Joystick buttons
+
+    // Low gear
+    new JoystickButton(driverJoystick, XboxController.Button.kA.value)
+      .whenPressed(new InstantCommand(() -> driveTrain.setGear(Value.kForward)));
+    // High gear
+    new JoystickButton(driverJoystick, XboxController.Button.kA.value)
+      .whenPressed(new InstantCommand(() -> driveTrain.setGear(Value.kReverse)));
+
+    // Gunner Joystick buttons
+
+    // Toggle Shooter
+    final JoystickButton toggleShooter = new JoystickButton(gunnerJoystick, XboxController.Button.kLeftBumper.value);
+    toggleShooter.whenHeld(enableShooterCmd);
+
+    // Deploy Intake
+    final JoystickButton deployIntake = new JoystickButton(gunnerJoystick, XboxController.Button.kA.value);
+    deployIntake.whenPressed(deployIntakeCmdGroup);
+
+    // Undeploy Intake
+    new JoystickButton(gunnerJoystick, XboxController.Button.kB.value)
+      .whenPressed(new InstantCommand(() -> intake.setPiston(Value.kReverse)));
+
+    // Enable Feeder
+    final JoystickButton enableFeeder = new JoystickButton(gunnerJoystick, XboxController.Button.kRightBumper.value);
+    enableFeeder.whileHeld(enableFeederCmd);
+  
+    // Enable auto-aim
+    final JoystickButton autoAim = new JoystickButton(gunnerJoystick, XboxController.Button.kX.value);
+    //autoAim.whileHeld();
+
+  
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -44,6 +45,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
     gearbox = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.Solenoid.Drive.GEARBOX_LOW, Constants.Solenoid.Drive.GEARBOX_HIGH);
 
+    gearbox.set(Value.kForward);
   }
 
   @Override
