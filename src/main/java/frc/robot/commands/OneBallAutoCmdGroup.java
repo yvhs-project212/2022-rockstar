44ae863cee750a91b3a5abbaa054976c2ef9da17
Project212Robotics <project212robotics@gmail.com>
2022-03-26 09:17:47 -0700

    WHY IS IT BROKEN?

--- src/main/java/frc/robot/commands/OneBallAutoCmdGroup.java
+++ src/main/java/frc/robot/commands/OneBallAutoCmdGroup.java
@@ -4,17 +4,21 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.AutonomousConstants;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.HangSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.StorageSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
+import frc.robot.subsystems.ShooterSubsystem.VelocityControlMode;
+import frc.robot.subsystems.StorageSubsystem.MotorSelection;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -22,8 +26,8 @@ import frc.robot.subsystems.TurretSubsystem;
 public class OneBallAutoCmdGroup extends SequentialCommandGroup {
   /** Creates a new AutonomousCmdGroup. */
 
-  public OneBallAutoCmdGroup(DrivetrainSubsystem drivetrainSubsystem, StorageSubsystem storageSubsystem, 
-  HangSubsystem hangSubsystem, IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem, TurretSubsystem turretSubsystem) {
+  public OneBallAutoCmdGroup(DrivetrainSubsystem drive, StorageSubsystem storage, 
+  HangSubsystem hang, IntakeSubsystem intake, ShooterSubsystem shooter, TurretSubsystem turret) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     //addRequirements(drivetrainSubsystem);
@@ -36,28 +40,28 @@ public class OneBallAutoCmdGroup extends SequentialCommandGroup {
        * You cannot go backwards
        * 
        */
-      new DriveForwardCmd(drivetrainSubsystem, storageSubsystem, hangSubsystem, 4),
+      new DriveForwardCmd(drive, storage, hang, -4),
       
       /**
        * Figure out how to do a ParallelDeadlineCommandGroup
        * 
        * So I can get out of shooting a ball once
        */
-      new ParallelCommandGroup(
-        new RunCommand(turretSubsystem::turretWithLimelight, turretSubsystem),
-        new EnableShooterCmd(shooterSubsystem), 
-        .until(condition)
-        .andTeh
-        new EnableFeederCmd(storageSubsystem)
-        ).withInterrupt(condition),
-        
-      new ParallelCommandGroup(
-        new RunCommand(shooterSubsystem::disable, shooterSubsystem),
-        new RunCommand(storageSubsystem::stopMotors , storageSubsystem)
-      ),
-      new RunCommand(shooterSubsystem::stopMotors),
+      new InstantCommand(() -> shooter.setVelocityControlMode(VelocityControlMode.LIMELIGHT)),
       
+      new ParallelCommandGroup(
+        new RunCommand(turret::turretWithLimelight, turret),
+        new EnableShooterCmd(shooter, null),
+        new WaitCommand(AutonomousConstants.FLYWHEEL_REV_TIME_SECONDS),
+        new EnableFeederCmd(storage))
+      .withTimeout(AutonomousConstants.TIMEOUT_SECONDS)
+      .andThen(
+        new InstantCommand(shooter::disable),
+        new InstantCommand(() -> storage.setMotors(MotorSelection.NONE, 0, 0))
+      ),
+    
       new PrintCommand("AutonomousCmdGroup ended!")
+      
     );
   }
 }
