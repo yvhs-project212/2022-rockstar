97f3285e35a9152daa6b966022c39bc2e6511d58
Project212Robotics <project212robotics@gmail.com>
2022-03-25 22:33:56 -0700

    Removing excess code

--- src/main/java/frc/robot/subsystems/HangSubsystem.java
+++ src/main/java/frc/robot/subsystems/HangSubsystem.java
@@ -10,7 +10,6 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.HangConstants;
